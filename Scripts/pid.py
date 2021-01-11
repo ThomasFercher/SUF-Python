754eb9a84f6414492f92095f0cf10593200a254e
Thomas <thomas.fercher@htl-klu.at>
2021-01-11 18:21:20 +0100

    pid

--- Scripts/pid.py
+++ Scripts/pid.py
@@ -1,20 +1,22 @@
 from simple_pid import PID
 import threading, time
+import sensors
 
 
-pid = PID(1, 0.1, 0.05, setpoint=1,sample_time=0.1,)
+pid = PID(1, 0.1, 0.05, setpoint=30,sample_time=0.1,)
 
 def setSetpoint(value):
     pid.setpoint = value
     
 
 # assume we have a system we want to control in controlled_system
-v = 1
+temperature = 30
+old_temperature = 30;
 pid.sample_time = 1
 
 ta = pid.sample_time
 
-def do_every(period,f,*args):
+def do_every(period,f):
     def g_tick():
         t = time.time()
         while True:
@@ -23,21 +25,29 @@ def do_every(period,f,*args):
     g = g_tick()
     while True:
         time.sleep(next(g))
-        f(*args)
+        f()
 
 def cycle():
-    temperature,humidity = readDHT()
+	global temperature
+	old_temperature = temperature
+	
+	dht = sensors.readDHT()
+	if not(dht is None):
+		temperature,humidity = sensors.readDHT()
+	else:
+		temperature = old_temperature
     # compute new ouput from the PID according to the systems current value
-    control = pid(temperature)
+	control = pid(temperature)
 
     # feed the PID output to the system and get its current value
     
-    print('{} ({:.4f})'.format(s,time.time()))
-    print(control)
-    print(temperature)
+  
+	print(control)
+	print(temperature)
 
 
+while True:
+	cycle()
+	time.sleep(1)
 
-
-do_every(ta,cycle)
 
--- Scripts/sensors.py
+++ Scripts/sensors.py
@@ -4,14 +4,21 @@ import adafruit_dht
 #DHT22
 dhtDevice = adafruit_dht.DHT22(board.D18)
 
-
-
 def readDHT():
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
-    print(temperature)
-    print(humidity)
-    return temperature,humidity
-
-
-readDHT()
+   
+	
+	try:
+		# Print the values to the serial port
+		temperature = dhtDevice.temperature
+		humidity = dhtDevice.humidity
+		return temperature,humidity
+	except RuntimeError as error:
+		# Errors happen fairly often, DHT's are hard to read, just keep going
+		print(error.args[0])
+		
+	except Exception as error:
+		dhtDevice.exit()
+		raise error
+ 
+    
+	
